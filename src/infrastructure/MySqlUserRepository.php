0772ea00bd93c347b5f7964ef009a64b1643fda2
Luka Maret <maret.luka@gmail.com>
2022-09-30 20:31:50 +0200

    feat: add example files

---
+++ src/infrastructure/MySqlUserRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\infrastructure;
+
+use App\application\UserRepository;
+
+class MySqlUserRepository implements UserRepository {
+
+}
---
+++ src/model/User.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\model;
+
+class User {
+
+}
