54cbdf99b4061933f8cced4022cb3029edb76d80
Lilian BAUDRY <lilian.baudry21@gmail.com>
2022-11-02 01:49:45 +0100

    feat: added scrollbar for carousel navigation

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,37 +1,77 @@
+
 // relocate the slider to the specified position
-const relocate = (element, pourcentage) =>{
-	element.scrollLeft = pourcentage * (element.scrollWidth - element.clientWidth)
+const relocateSlider = (pourcentage) =>{
+	sliderUI.slider.scrollLeft = pourcentage * (sliderUI.slider.scrollWidth - sliderUI.slider.clientWidth)
+	selectMiddleCard();
 };
 
-let slider = document.querySelector(".carousel__slider");
+// relocate the scrollbar to match the slider position
+const relocateScrollbar = () =>{
+	sliderUI.cursor.value = getHorizontalRatio()*100;
+	selectMiddleCard();
+};
 
-let sliderController = {
+// get the current position of the slider
+const getHorizontalRatio = () => {
+	return sliderUI.slider.scrollLeft / (sliderUI.slider.scrollWidth - sliderUI.slider.clientWidth)
+}
+
+// give to the middle card the class "card--middle"
+const selectMiddleCard = () => {
+	let index = Math.round(getHorizontalRatio() * (sliderUI.cards.length - 1));
+	sliderUI.middleCard?.classList.remove("card--middle");
+	sliderUI.middleCard = sliderUI.cards[index];
+	sliderUI.middleCard.classList.add("card--middle");
+}
+
+// --------------------- constants --------------------- //
+
+const sliderUI = {
+	slider : document.querySelector(".carousel__slider"),
+	cards : document.querySelectorAll(".card"),
+	cursor : document.querySelector(".scrollbar__cursor"),
+	middleCard : null,
+}
+
+const sliderController = {
 	isMouseDown: false,
-	scrollLeft: slider.offsetLeft,
+	scrollLeft: sliderUI.slider.offsetLeft,
 	startPosX: 0,
 };
 
-slider.addEventListener("mousedown", (event) => {
+// ------------------ Event Listeners ------------------ //
+
+sliderUI.slider.addEventListener("mousedown", (event) => {
 	sliderController.isMouseDown = true;
-	sliderController.scrollLeft = slider.scrollLeft;
-	sliderController.startPosX = event.pageX - slider.offsetLeft;
+	sliderController.scrollLeft = sliderUI.slider.scrollLeft;
+	sliderController.startPosX = event.pageX - sliderUI.slider.offsetLeft;
 });
 
-slider.addEventListener("mousemove", (event) => {
+sliderUI.slider.addEventListener("mousemove", (event) => {
 	if (!sliderController.isMouseDown)
 		return;
 
-	let diffX = event.pageX - slider.offsetLeft;
+	let diffX = event.pageX - sliderUI.slider.offsetLeft;
 	let changeX = diffX - sliderController.startPosX;
-	slider.scrollLeft = sliderController.scrollLeft - changeX;
+
+	sliderUI.slider.scrollLeft = sliderController.scrollLeft - changeX;
+
+	relocateScrollbar();
 });
 
-slider.addEventListener("mouseup", (event) => {
+sliderUI.slider.addEventListener("mouseup", (event) => {
 	sliderController.isMouseDown = false;
 });
 
-slider.addEventListener("mouseleave", (event) => {
+sliderUI.slider.addEventListener("mouseleave", (event) => {
 	sliderController.isMouseDown = false;
 });
 
-relocate(slider, 0.5)
+sliderUI.cursor.addEventListener("input", (event) => {
+	relocateSlider(sliderUI.cursor.value/100)
+});
+
+// ------------------ Inititialization ----------------- //
+
+relocateSlider(0.5)
+relocateScrollbar();
