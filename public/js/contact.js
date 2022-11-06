2ede8b39b7109cfd8e81678f0f61e509cf512342
Luka Maret <maret.luka@gmail.com>
2022-11-06 13:43:36 +0100

    script: change js structure

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,12 +1,4 @@
-let nameInput = document.getElementById('name');
-let descriptionInput = document.getElementById('description');
-let typeInput = document.getElementById('type');
-let emailInput = document.getElementById('email');
-let submitButton = document.getElementsByClassName('btn')[0];
-let errorPopup = document.getElementsByClassName('error-popup')[0];
-let errorPopupClose = document.getElementsByClassName('error-popup__close-button')[0];
-
-function checkInput(element) {
+function checkInputValidity(element) {
 	element.classList.add('form__element--invalid');
 	if (element.value.length > 0) {
 		element.classList.remove('form__element--invalid');
@@ -15,7 +7,7 @@ function checkInput(element) {
 	return false;
 }
 
-function checkEmail(emailInput) {
+function checkEmailValidity(emailInput) {
 	emailInput.classList.add('form__element--invalid');
 	if (emailInput.value.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
 		emailInput.classList.remove('form__element--invalid');
@@ -24,28 +16,62 @@ function checkEmail(emailInput) {
 	return false;
 }
 
-nameInput.addEventListener('focusout', () => checkInput(nameInput));
-descriptionInput.addEventListener('focusout', () => checkInput(descriptionInput));
-typeInput.addEventListener('focusout', () => checkInput(typeInput));
-emailInput.addEventListener('focusout', () => checkEmail(emailInput));
+function addFieldsListeners() {
 
-errorPopupClose.addEventListener('click', () => errorPopup.classList.remove('error-popup--visible'));
+	fields.forEach(field => document.getElementById(field.name)
+		.addEventListener('focusout', () => field.validation(document.getElementById(field.name))));
+}
 
-submitButton.addEventListener('click', function (e) {
+function addPopupCloseListener() {
+	let errorPopup = document.getElementsByClassName('error-popup')[0];
+	document.getElementsByClassName('error-popup__close-button')[0]
+		.addEventListener('click', () => errorPopup.classList.remove('error-popup--visible'));
+}
 
-	if (checkInput(nameInput) && checkInput(descriptionInput) && checkInput(typeInput) && checkEmail(emailInput)) {
+function performSubmit(e) {
+
+	if (fields.every(field => field.validation(document.getElementById(field.name)))) {
 		return;
 	}
 
 	e.preventDefault();
 
-	let message = 'Champs invalides : <br>';
-	message += !checkInput(nameInput) ? 'nom, ' : '';
-	message += !checkInput(descriptionInput) ? 'description, ' : '';
-	message += !checkInput(typeInput) ? 'objet, ' : '';
-	message += !checkEmail(emailInput) ? 'email' : '';
+	let errorPopup = document.getElementsByClassName('error-popup')[0];
+	errorPopup.getElementsByClassName("error-popup__content")[0].innerHTML = fields.filter(field => !field.validation(document.getElementById(field.name)))
+		.map(field => field.error)
+		.join('<br>');
 
-	errorPopup.getElementsByClassName("error-popup__content")[0].innerHTML = message;
 	errorPopup.classList.add('error-popup--visible');
-	setTimeout(() => errorPopup.classList.remove('error-popup--visible'), 3_000);
-});
+	setTimeout(() => errorPopup.classList.remove('error-popup--visible'), 5_000);
+}
+
+function addSubmitListener() {
+	document.getElementsByClassName('btn')[0].addEventListener('click', (e) => performSubmit(e));
+}
+
+let fields = [
+	{
+		name: 'name',
+		error: 'Le nom doit contenir au moins 1 caractères',
+		validation: checkInputValidity
+	},
+	{
+		name: 'description',
+		error: 'La description doit contenir au moins 1 caractères',
+		validation: checkInputValidity
+	},
+	{
+		name: 'type',
+		error: 'Le type doit contenir au moins 1 caractères',
+		validation: checkInputValidity
+	},
+	{
+		name: 'email',
+		error: 'L\'email doit être valide',
+		validation: checkEmailValidity
+	}
+];
+
+addFieldsListeners();
+addPopupCloseListener();
+addSubmitListener();
