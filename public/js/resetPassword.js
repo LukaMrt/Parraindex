b79b5c3658fc7ea600122dc01ab80824116283c0
Luka Maret <maret.luka@gmail.com>
2022-11-14 08:45:11 +0100

    feat: add error popups to connection forms

--- public/js/resetPassword.js
+++ public/js/resetPassword.js
@@ -3,8 +3,7 @@ import {Field, registerForm} from "./formValidator.js";
 let defaultValidation = (element) => element.checkValidity();
 
 let fields = [
-	new Field('login', 'L\'email doit être valide', defaultValidation),
-	new Field('password', 'Le mot de passe doit contenir au moins 1 caractère', defaultValidation),
+	new Field('email', 'L\'email doit être valide', defaultValidation),
 ];
 
 registerForm(document.querySelector('.form'), fields);
