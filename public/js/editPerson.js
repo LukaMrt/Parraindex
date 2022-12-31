124d03c6a054c3bdd512549ab0fdd82285ae6367
Lilian BAUDRY <lilian.baudry21@gmail.com>
2022-12-31 03:15:29 +0100

    feat: UI/UX optimization

--- public/js/editPerson.js
+++ public/js/editPerson.js
@@ -1,65 +1,173 @@
-import {triggerErrorPopup , triggerSuccessPopup} from "./popup/popups.js";
+import { triggerErrorPopup, triggerSuccessPopup } from "./popup/popups.js";
 
 const field = {
-	bio : document.querySelector("#bio-field"),
-	about : document.querySelector("#about-field"),
-	colors : document.querySelector("#color-field"),
-	contact : document.querySelector("#contact-field"),
-	picture : document.querySelector("#img-uploader"),
+	form: document.querySelector(".edit-person"),
+	bio: document.querySelector("#bio-field"),
+	about: document.querySelector("#about-field"),
+	colors: document.querySelector("#color-field"),
+	contact: document.querySelector("#contact-field"),
+	picture: document.querySelector("#img-uploader"),
+	lastName: document.querySelector("#lastname-field"),
+	firstName: document.querySelector("#firstname-field"),
+	colorPicker: document.querySelector("#color-picker"),
+	radioColorPicker: document.querySelector("#radio-color-picker"),
+	characteristics: document.querySelector(".information__contact__characteristics"),
 }
 
 const preview = {
-	bio : document.querySelector(".card__description"),
-	color : document.querySelector(".card__banner"),
-	contacts : document.querySelector("card__social-network"),
-	picture : document.querySelector(".card__picture"),
-	overlayPicture : document.querySelector(".card__picture__overlay"),
+	bio: document.querySelector(".card__description"),
+	color: document.querySelector(".card__banner"),
+	contacts: document.querySelector("card__social-network"),
+	picture: document.querySelector(".card__picture"),
+	overlayPicture: document.querySelector(".card__picture__overlay"),
+	lastName: document.querySelector(".card__last-name"),
+	firstName: document.querySelector(".card__first-name"),
+
+	characteristicType: document.querySelectorAll(".social-network"),
+	characteristics: document.querySelector(".card__social-network"),
 }
 
 const pictureModal = {
-	container : document.querySelector(".edit-picture"),
-	imagePreview : document.querySelector(".edit-picture__preview__picture"),
-	imageUploader : document.querySelector("#img-uploader"),
-	closeButton : document.querySelector(".edit-picture__close"),
+	container: document.querySelector(".edit-picture"),
+	imagePreview: document.querySelector(".edit-picture__preview__picture"),
+	imageUploader: document.querySelector("#img-uploader"),
+	closeButton: document.querySelector(".edit-picture__close"),
+}
+
+const action = {
+	delete: document.querySelector("#delete-person"),
+	save: document.querySelector("#save-person"),
 }
 
 const initialPicture = preview.picture.src;
-const deleteButton = document.querySelector("#delete-person");
+const personId = Number(document.querySelector(".card__identifiant").textContent);
 
-function initColor(){
+/**
+ * Encode every field of the form in JSON
+ * @returns {Promise<string>} The JSON string
+ */
+async function formEncodeJson() {
+
+	let formData = new FormData(field.form);
+
+	await new Promise((resolve) => {
+		let reader = new FileReader();
+
+		if (!field.picture.files[0]) {
+			resolve();
+		}
+
+		reader.readAsDataURL(field.picture.files[0]);
+
+		reader.onload = async (e) => {
+			formData.append("person-picture", e.target.result);
+			resolve();
+		}
+	});
+
+
+
+	return JSON.stringify(Object.fromEntries(formData));
+}
+
+/**
+ * Call method to update the preview and init the color picker
+ */
+function init() {
+	initColor();
+	updatePreview();
+
+	if (personId === 0){
+		action.save.textContent = "Créer";
+		action.delete.style.display = "none";
+	}
+}
+
+/**
+ * Select the color of the banner in the color picker
+ */
+function initColor() {
 
 	let bannerColor = getComputedStyle(preview.color).getPropertyValue("background-color")
 	pictureModal.imagePreview.style.backgroundColor = bannerColor;
-	for (const color of field.colors.children){
+
+	let find = false;
+
+	for (const color of field.colors.children) {
 		let selectorColor = getComputedStyle(color).getPropertyValue("background-color")
-		if (selectorColor === bannerColor){
+		if (selectorColor === bannerColor) {
 			color.children[0].checked = true;
+			find = true;
 		}
 	}
 
+	if (!find) {
+		field.radioColorPicker.checked = true;
+		field.colorPicker.value = bannerColor;
+		field.colorPicker.parentElement.style.backgroundColor = bannerColor;
+	}
+
 }
+
+/**
+ * Update the preview with the characteristics of the person
+ */
+function updateCharacteristicPreview(){
+
+	let newCharacteristic = [];
+	let characteristicsCounter = 0
+
+	for (var i = 0; i < field.characteristics.children.length; i++) {
+
+
+		let characteristic = field.characteristics.children[i];
+		let visibility = characteristic.querySelector("input[type=checkbox]");
+		let value = characteristic.querySelector(".characteristic__value").value;
+
+		if (visibility.checked && characteristicsCounter>=3){
+			visibility.checked = false;
+			triggerErrorPopup(["Vous ne pouvez pas avoir plus de 3 réseaux sociaux affichés"]);
+		}
+		else if (visibility.checked){
+			let previewCharacteristic = preview.characteristicType[i].cloneNode(true);
+			previewCharacteristic.href += value;
+			newCharacteristic.push(previewCharacteristic);
+			characteristicsCounter++;
+		}
+	}
+
+	preview.characteristics.replaceChildren(...newCharacteristic);
+}
+
 /**
  * Update the card preview with the values of the form fields
  */
-function updatePreview(){
+function updatePreview() {
 	preview.bio.textContent = field.bio.value;
 
 	let bannerColor = field.colors.querySelector("input:checked").parentElement.style.backgroundColor
 
 	preview.color.style.backgroundColor = bannerColor;
 	pictureModal.imagePreview.style.backgroundColor = bannerColor;
+
+	if(field.lastName && field.firstName){
+		preview.lastName.textContent = field.lastName.value;
+		preview.firstName.textContent = field.firstName.value;
+	}
+
+	updateCharacteristicPreview();
+
 }
 
-preview.picture.addEventListener("mouseover", function(){
+preview.picture.addEventListener("mouseover", () => {
 	preview.overlayPicture.classList.add("overlay--active");
 	preview.picture.classList.add("overlay--active");
 })
 
-preview.picture.addEventListener("mouseout", function(){
+preview.picture.addEventListener("mouseout", () => {
 	preview.overlayPicture.classList.remove("overlay--active");
 	preview.picture.classList.remove("overlay--active");
 })
-
 
 /**
  * Open the picture modal and add event listeners to close it
@@ -68,32 +176,37 @@ preview.picture.addEventListener("click", (event) => {
 
 	pictureModal.container.classList.add("edit-picture--active");
 	event.stopPropagation();
-	
+
+	pictureModal.closeButton.addEventListener("click", closeByButton);
 	document.addEventListener("keydown", closeByKey);
 	document.addEventListener("click", closeByClick);
 
-	pictureModal.closeButton.addEventListener("click", e => {
+
+	function closeByButton(e) {
 		e.preventDefault();
 		pictureModal.container.classList.remove("edit-picture--active");
-		document.removeEventListener("keydown", closeByKey);
-		document.removeEventListener("click", closeByClick);
-	}, {once : true});
+		deleteEventListener();
+	}
 
-	function closeByKey(e){
-		if (e.key === "Escape"){
+	function closeByKey(e) {
+		if (e.key === "Escape") {
 			pictureModal.container.classList.remove("edit-picture--active");
-			document.removeEventListener("keydown", closeByKey);
-			document.removeEventListener("click", closeByClick);
+			deleteEventListener();
 		}
 	}
 
 	function closeByClick(e) {
-		if (!e.path.includes(pictureModal.container)){
+		if (!e.path.includes(pictureModal.container)) {
 			pictureModal.container.classList.remove("edit-picture--active");
-			document.removeEventListener("click", closeByClick);
+			deleteEventListener();
 		}
 	}
 
+	function deleteEventListener() {
+		document.removeEventListener("keydown", closeByKey);
+		document.removeEventListener("click", closeByClick);
+		document.removeEventListener("click", closeByButton);
+	}
 });
 
 /**
@@ -104,10 +217,10 @@ field.picture.addEventListener("input", e => {
 	let reader = new FileReader();
 	let picture = field.picture.files[0];
 
-	if (picture.size > 5_000 * 1024){
+	if (picture.size > 5_000 * 1024) {
 
 		// temporary alert, will be replaced by a modal
-		alert("La taille de l'image est trop grande !");
+		alert("Attention, la taille de l'image ne doit pas dépasser 5Mo");
 
 		pictureModal.imageUploader.value = "";
 		pictureModal.imagePreview.src = initialPicture;
@@ -115,42 +228,93 @@ field.picture.addEventListener("input", e => {
 
 		return;
 	}
-	
+
 	reader.readAsDataURL(picture);
-	reader.onload = function(e) {
+	reader.onload = function (e) {
 		pictureModal.imagePreview.src = e.target.result;
 		preview.picture.src = e.target.result;
 	};
 });
 
+field.colorPicker.addEventListener("input", e => {
+	field.radioColorPicker.checked = true;
+	field.radioColorPicker.value = e.target.value;
+	e.target.parentElement.style.backgroundColor = e.target.value;
+});
 
-deleteButton?.addEventListener("click", async e => {
+action.save.addEventListener("click", async e => {
 
 	e.preventDefault();
-	
-	if (confirm("Êtes-vous sûr de vouloir supprimer cette personne ?")){
-		
-		let request = await fetch(window.location.href, { method : "DELETE" });
 
-		if (request.status !== 500){
-			let response = await request.json();
+	let request = await fetch(window.location.href, {
+		method: (personId === 0) ? "POST" : "PUT",
+		headers: {
+			"content-type": "application/json"
+		},
+		body: await formEncodeJson()
+	});
 
-			console.log(response);
+	if (request.ok){
 
-			if (response.success){
-				triggerSuccessPopup(response.messages);
-				setTimeout(() => {
-					window.location.href = response.redirect;
-				}, response.redirectDelay);
-			}else{
-				triggerErrorPopup(response.messages);
+		let response = await request.json();
+
+		if (response.code === 200){
+			if (personId === 0){
+				field.form.reset();
+
+				preview.picture.src = initialPicture;
+				pictureModal.imagePreview.src = initialPicture;
+
+				init();
 			}
+			triggerSuccessPopup(response.messages);
 		}else{
-			triggerErrorPopup(["Une erreur est survenue lors de la suppression de la personne"]);
+			triggerErrorPopup(response.messages);
 		}
+
+
+	}else{
+		triggerErrorPopup(["Une erreur est survenue lors de la requête au serveur"]);
 	}
 });
 
-initColor();
+action.delete?.addEventListener("click", async e => {
+
+	e.preventDefault();
+
+	if(!e.target.classList.contains("sure")){
+		e.target.classList.add("sure");
+		setTimeout(() => {
+			e.target.textContent = "Êtes-vous sûr ?";
+		}, 200);
+		return;
+	}
+
+	let request = await fetch(window.location.href, {
+		method: "DELETE",
+	});
+
+	if (request.ok){
+
+		let response = await request.json();
+
+		if (response.code === 200){
+			triggerSuccessPopup(response.messages);
+
+			setTimeout(() => {
+				window.location.href = response.redirect;
+			}, response.redirectDelay);
+
+		}else{
+			triggerErrorPopup(response.messages);
+		}
+
+	}else{
+		triggerErrorPopup(["Une erreur est survenue lors de la requête au serveur"]);
+	}
+});
+
+
+init();
 
 document.addEventListener("input", updatePreview);
