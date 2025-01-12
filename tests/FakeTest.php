c704eab8763987136f7e373bf0bccb5f16620230
LukaMrt <maret.luka@gmail.com>
2025-01-12 17:35:17 +0100

    fix issues

---
+++ tests/FakeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class FakeTest extends TestCase
+{
+    public function testFake(): void
+    {
+        $this->assertTrue(true);
+    }
+}
