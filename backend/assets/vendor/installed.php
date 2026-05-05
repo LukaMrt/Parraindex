76a8cb764734341415241d3429c2f24a2f28a7c8
LukaMrt <maret.luka@gmail.com>
2026-05-05 08:26:56 +0200

    Update dependencies

---
+++ backend/assets/vendor/installed.php
@@ -0,0 +1,23 @@
+<?php return array (
+  'react' => 
+  array (
+    'version' => '19.2.5',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'react-dom' => 
+  array (
+    'version' => '19.2.5',
+    'dependencies' => 
+    array (
+      0 => 'react',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+);
