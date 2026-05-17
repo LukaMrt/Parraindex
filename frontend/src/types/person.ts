43f2160b2b21905f3537ffb2bb713f1589ba3306
Donatien Vachette <donatien.vachette@hotmail.com>
2026-05-17 17:17:01 +0200

    Interface Filiere frontend

--- frontend/src/types/person.ts
+++ frontend/src/types/person.ts
@@ -22,6 +22,7 @@ export interface Person {
   godFathers: Sponsor[];
   godChildren: Sponsor[];
   characteristics: Characteristic[];
+  filieres: Filiere[];
 }
 
 export interface PersonRequest {
@@ -30,4 +31,10 @@ export interface PersonRequest {
   startYear: number;
   biography: string | null;
   description: string | null;
+}
+
+export interface Filiere {
+  name: string;
+  startYear: number;
+  endYear: number | null;
 }
