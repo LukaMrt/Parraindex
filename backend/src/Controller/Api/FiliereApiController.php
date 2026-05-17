a2b5178e0e03f38f62990f8ea83870e92e30b603
Donatien Vachette <donatien.vachette@hotmail.com>
2026-05-17 18:09:54 +0200

    Route pour lister toutes les filieres

---
+++ backend/src/Controller/Api/FiliereApiController.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Api;
+
+use App\Api\ApiResponse;
+use App\Dto\Person\FiliereDto;
+use App\Repository\FiliereRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ContactApiController extends AbstractController {
+    public function __construct(
+        private readonly FiliereRepository $filiereRepository,
+    ) {
+    }
+
+    #[Route('/api/filieres', name: 'api_filieres_list', methods: ['GET'])]
+    public function listFilieres(): JsonResponse
+    {
+        $filieres = $this->filiereRepository->findAll();
+
+        $ret = array_map(
+            static fn($filiere) => $filiere->getName(),
+            $filieres,
+        );
+
+        return ApiResponse::success($ret);
+    }
+}
